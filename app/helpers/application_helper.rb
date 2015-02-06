a71c6ee781b59c8adcc5dbabcc4eb5fc51375352
Andrew Callahan <andrew14callahan@gmail.com>
2015-02-06 22:41:29 +0000

    Add full_title helper function in ApplicationHelper class to define page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Returns the full title for any given page on my site.
+  def full_title(page_title = '')
+    base_title = "Facebook App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
 end
