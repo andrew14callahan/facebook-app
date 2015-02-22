f632449db038f7629a0f631c79ef26a8ad286f61
andrew14callahan <andrew14callahan@gmail.com>
2015-02-21 16:04:11 -0800

    Add application helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title for each page.
+	def full_title(page_title = '')
+		base_title = "Facebook App"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
